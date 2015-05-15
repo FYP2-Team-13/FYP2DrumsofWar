b4e25be8fe2de2e617dd4c0dcb0849cec6b81809
Bznboy <Bznsin@hotmail.com>
2015-05-15 13:59:34 +0800

    Added Basic Drum Beat
    Basic Drum Beat with no sequencing yet

---
+++ Drums of War/Assets/Scripts/BeatScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BeatScript {
+
+	public enum BeatType
+	{
+		Beat_Rest = 0,
+		Beat_Snare,
+		Beat_Tom,
+		Beat_Hithat,
+		Beat_Bass,
+		Beat_Number
+	}
+
+	BeatType type;
+
+	public void SetBeatType (BeatType type)
+	{
+		this.type = type;
+	}
+
+	public BeatType GetBeatType ()
+	{
+		return type;
+	}
+}
