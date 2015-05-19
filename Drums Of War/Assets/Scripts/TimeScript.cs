8a6e31f0f362ae7b1203facda6bfdfdfb994386d
Bznboy <Bznsin@hotmail.com>
2015-05-19 10:52:41 +0800

    Short Update for Beats

--- Drums Of War/Assets/Scripts/TimeScript.cs
+++ Drums Of War/Assets/Scripts/TimeScript.cs
@@ -7,25 +7,30 @@ public class TimeScript : MonoBehaviour {
 	float time = 0.0f;
 	public GameObject Beat;
 	bool keydown = false;
+	//public GameObject DebugText;
+	//TextEditor Text;
 
 	bool Endbeat = true;
 
 	public KeyCode Hihats, Snare, Bass, Toms;
 	// Use this for initialization
 	void Start () {
+		 //Text = DebugText.GetComponent<TextEditor> ();
 	}
 
 	bool BeatCheck ()
 	{
+		if (TheBeat.GetBeatType () == BeatScript.BeatType.Beat_Rest)
+			return false;
 		if (Endbeat) {
-			if (time < 0.1249f) { //if the key hits before the "beat" ended
+			if (time < 0.2f) { //if the key hits before the "beat" ended
 				//insert Beat Script sending
 				return true;
 			} else {
 				return false;
 			}
 		} else {
-			if (time > 0.1249f) { // if the key hits after the "beat starts"
+			if (time > 0.05f) { // if the key hits after the "beat starts"
 				//insert Beat Script sending
 				return true;
 			} else {
@@ -37,10 +42,12 @@ public class TimeScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		time += Time.deltaTime;//Time Update
+		
+		GUI.Label(new Rect (0, 100, 100, 100), time.ToString());
+
 		if (time > 0.249f) { // Check if the beat has ended
 			//print (time);
 			time = 0.0f; // reset time
-
 			if (Endbeat)
 			{
 				if (TheBeat.GetBeatType() == BeatScript.BeatType.Beat_Rest)
