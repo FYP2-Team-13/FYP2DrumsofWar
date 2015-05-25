7610ff1e0f94f565193a4bc134cbe261667762e6
Xeznom <Xeznom@gmail.com>
2015-05-26 00:23:56 +0800

    Simple Main Menu Set-up
    edited the main menu empty scene
    added buttons, titles, background
    all buttons showing is working
    set-up the build settings to have the buttons to work
    added a exit menu in the main menu

---
+++ Drums Of War/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour {
+
+	public Canvas QuitMenu;
+	public Button Play;
+	public Button Exit;
+
+	// Use this for initialization
+	void Start () {
+		QuitMenu = QuitMenu.GetComponent<Canvas>();
+		Play = Play.GetComponent<Button>();
+		Exit = Exit.GetComponent<Button> ();
+		QuitMenu.enabled = false;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void ExitPress()
+	{
+		QuitMenu.enabled = true;
+		Play.enabled = false;
+		Exit.enabled = false;
+	}
+
+	public void NoPress()
+	{
+		QuitMenu.enabled = false;
+		Play.enabled = true;
+		Exit.enabled = true;	
+	}
+
+	public void PlayPress()
+	{
+		Application.LoadLevel ("Gameplay");
+	}
+
+	public void YesPress()
+	{
+		UnityEditor.EditorApplication.isPlaying = false;
+		Application.Quit ();
+	}
+}
