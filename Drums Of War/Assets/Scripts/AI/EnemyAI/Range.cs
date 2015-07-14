8a9bd172e52cb6f0b44e70d089b6a7741707560f
AliceLied <maive_5566@hotmail.com>
2015-07-14 10:22:45 +0800

    z value to 0

--- Drums Of War/Assets/Scripts/AI/EnemyAI/Range.cs
+++ Drums Of War/Assets/Scripts/AI/EnemyAI/Range.cs
@@ -6,10 +6,6 @@ public class Range : MonoBehaviour {
 	public GameObject projectile;
 
 	// Use this for initialization
-	void Start () {
-		var script = this.gameObject.GetComponent<AI> ();
-		script.Set (0f, 1f, 5.0f, 1f, 10f, -1);
-	}
 	
 	// Update is called once per frame
 	void Update () {
